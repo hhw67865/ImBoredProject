6f364119de1a2d12e5c659427eca16867c7bb6a7
Henry Wu <hhw67865@gmail.com>
2022-10-24 16:37:19 -0400

    added basic html for different questions, hide css.

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-
-fetch("http://www.boredapi.com/api/activity?type=recreational")
+fetch("http://www.boredapi.com/api/activity?type=${}")
 .then(response => response.json())
 .then(boredActivity => {
     console.log(boredActivity);
 })
+
+//GLOBAL VARIABLES
+
+
+let username;
+
+//WELCOME FORM EVENT LISTENER
+const welcomeForm = document.querySelector("#welcomeForm");
+welcomeForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const welcomePage = document.querySelector(".welcome-page");
+    welcomePage.classList.add("hide");
+
+    const categoryPage = document.querySelector(".category-page");
+    categoryPage.classList.remove("hide");
+
+    let username = e.target.name.value;
+    // console.log(username);
+
+    welcomeForm.reset();
+
+    const body = document.querySelector("body");
+    const mainScript = document.createElement("script");
+    mainScript.src = "mainscript.js"
+    body.append(mainScript);
+
+});
